01e8093b3528b17d8b29e29391b4ee40797ece49
istiaqueahmedarik <69030256+istiaqueahmedarik@users.noreply.github.com>
2021-10-06 10:07:01 +0600

    Create wordsnack.js

---
+++ commands/wordsnack.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch')
+module.exports.run = async (client,message,args,queue,searcher)=>{
+  console.log(message.channel.id)
+  const vc = message.member.voice.channel;
+  if(!vc) return message.channel.send("vc te join ho")
+  
+  fetch(`https://discord.com/api/v8/channels/${vc.id}/invites`,{
+    method:"POST",
+    body:JSON.stringify({
+      max_age:86400,
+      max_uses:0,
+      target_application_id:"879863976006127627",
+      target_type:2,
+      temporary:false,
+      validate:null
+    }),
+    headers:{
+      "Authorization":`Bot ${process.env.DISCORD}`,
+      "Content-Type":"application/json"
+    }
+  }).then(res=>res.json())
+  .then(inv=>{
+    if(!inv.code) return message.channel.send("Prblm ase!!")
+    message.channel.send(`Col eksathe game kheli :V https://discord.com/invite/${inv.code}`)
+  })
+ 
+}
+
+module.exports.config={
+  name:"wordsnack",
+  aliases:['ws','wordsnack']
+}
